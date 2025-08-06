4872dcf91c447df6f2c60f5deed6ca283c59ee48
bkinglucas97 <bking.lucas97@gmail.com>
2025-08-06 18:59:12 -0300

    wip: reading rust args from env

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,21 +1,30 @@
-fn divide(x: u64, y: u64) -> u64 {
-    assert!(y != 0);
+//fn divide(x: u64, y: u64) -> u64 {
+//    assert!(y != 0);
+//
+//    x / y
+//}
+//
+//fn foo() -> u64 {
+//    64_u64
+//}
+use std::env;
+use std::str::FromStr;
 
-    x / y
-}
-
-fn foo() -> u64 {
-    64_u64
-}
 fn main() {
-    //divide(10, 0);
-    println!("{}", foo());
-    println!("Hello, World!");
+    let mut my_vec: Vec<u64> = Vec::new();
+    for arg in env::args().skip(1) {
+        //println!("arg: {}", std::any::type_name_of_val(&arg)); 
+        // alloc::string::String
+        my_vec.push(u64::from_str(&arg).expect("Cannot convert string to u64"));
+    }
 
+    for x in my_vec.iter() {
+        println!("Vec value: {}", x);
+    }
 }
 
-#[test]
-fn test_foo() {
-    assert_eq!(64_u64, foo());
-}
+//#[test]
+//fn test_foo() {
+//    assert_eq!(64_u64, foo());
+//}
 
